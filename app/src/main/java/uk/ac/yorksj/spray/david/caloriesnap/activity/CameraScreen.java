bc13c6d69eda90076656885b7127a9e557f1323f
David <Dspray95@gmail.com>
2017-10-27 14:28:07 +0100

    initial commit

---
+++ app/src/main/java/uk/ac/yorksj/spray/david/caloriesnap/activity/CameraScreen.java
@@ -0,0 +1,15 @@
+package uk.ac.yorksj.spray.david.caloriesnap.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uk.ac.yorksj.spray.david.caloriesnap.R;
+
+public class CameraScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_screen);
+    }
+}
